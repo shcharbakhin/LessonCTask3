c6004ceff1f61796e335837bb8709dbe6242d963
weber02 <weber02@mail.ru>
2022-09-23 22:20:05 +0300

    10 задача решена

---
+++ задача 10/Program.cs
@@ -0,0 +1,21 @@
+﻿int count = 10;
+int[] array = new int[count];
+int find = new Random().Next(1, 20);
+int index = 0;
+
+while (index < count);
+{
+    array[index] = new Random().Next(1, 20);
+    index += 1;
+}
+
+index = 0;
+while (index < count)
+{
+    if (array[index] = find)
+    {
+        Console.WriteLine("Yes");
+    }
+    index += 1;
+}
+Console.WriteLine("end");
