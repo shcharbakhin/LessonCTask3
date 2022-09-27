730d2383b5cead86984cae395176b0914337576b
weber02 <weber02@mail.ru>
2022-09-27 20:17:15 +0300

    cj[hfy

---
+++ зад 13/Program.cs
@@ -0,0 +1,18 @@
+﻿{
+    int A, B, C, D, E;
+    Console.WriteLine("Введите трехзначное число");
+    A = Convert.ToInt32(Console.ReadLine());
+    B = A % 10;
+    C = (A / 10) * 10;
+    E = (A / 1000) * 1000;
+    if (100 > A)
+    {
+        Console.WriteLine("В числе нет третье цифры");
+    }
+    else
+    {
+        D = (A - B - C - E) / 100;
+        Console.Write("Третья цифра числа: ");
+        Console.WriteLine(D);
+    }
+}
